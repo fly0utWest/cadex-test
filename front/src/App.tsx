d233200951a55b4a1316011dee37cd537cf9fad6
Nikita Ablamskiy <shealtov@gmail.com>
2024-10-28 15:44:25 +0300

    Frontend template is added

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+    <header>hioho</header>
+    <main></main>
+    <footer></footer>
+    </>
+  );
+}
+
+export default App;
